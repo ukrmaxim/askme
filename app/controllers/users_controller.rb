c698644eea3f3c1129c3f9f9a72a57130c362fa6
ukrmaxim <ukrmaxim@protonmail.com>
2021-04-14 19:38:15 +0300

    Add sessions_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,74 @@
 class UsersController < ApplicationController
-  def index
-    @users = User.all
-  end
-
-  def new; end
+  before_action :load_user, except: [:index, :create, :new]
 
   def edit
   end
 
+  def create
+    # Если пользователь уже авторизован, ему не нужна новая учетная запись,
+    # отправляем его на главную с сообщением.
+    redirect_to root_url, alert: 'Вы уже авторизованы' if current_user.present?
+
+    @user = User.new(user_params)
+
+    if @user.save
+      # Если удалось, отправляем пользователя на главную с сообщение, что
+      # пользователь создан.
+      redirect_to root_url, notice: 'Регистрация успешна!'
+    else
+      # Если не удалось по какой-то причине сохранить пользователя, то рисуем
+      # (обратите внимание, это не редирект), страницу new с формой
+      # пользователя, который у нас лежит в переменной @user. В этом объекте
+      # содержатся ошибки валидации, которые выведет шаблон формы.
+      render 'new'
+    end
+  end
+
+  def index
+    @users = User.all
+  end
+
+  def new
+    # Если пользователь уже авторизован, ему не нужна новая учетная запись,
+    # отправляем его на главную с сообщением.
+    redirect_to root_url, alert: 'Вы уже авторизованы' if current_user.present?
+
+    @user = User.new
+  end
+
   def show
-    @user = User.find(params[:id])
-
-    @questions = @user.questions
-
-    # Болванка для нового вопроса
-    @new_question = Question.new
+    @questions = @user.questions.order(created_at: :desc)
+    @new_question = @user.questions.build
 
     @questions_count = @questions.count
     @unanswered_count = @questions.unanswered.count
     @answered_count = @questions.answered.count
   end
+
+  def update
+    if @user.update(user_params)
+      # Если удалось, отправляем пользователя на главную с сообщение, что
+      # данные профиля обновлены.
+      redirect_to user_path(@user), notice: 'Данные профиля обновлены!'
+    else
+      # Если не получилось, как и в create рисуем страницу редактирования
+      # пользователя, на которой нам будет доступен объект @user, содержащий
+      # информацию об ошибках валидации, которые отобразит форма.
+      render 'edit'
+    end
+  end
+end
+
+private
+
+def authorize_user
+  reject_user unless @user == current_user
+end
+
+def load_user
+  @user = User.find(params[:id])
+end
+
+def user_params
+  params.require(:user).permit(:email, :password, :password_confirmation, :name, :username, :avatar_url)
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
-  # Эти две строчки добавляют ресурсы для пользователей и вопросов. Ресурс — это
-  # набор путей для управления соответствующей моделью. Посмотрите, что напишет
-  # команда
-  resources :users, except: [:destroy]
-  resources :questions
   # Это правило говорит: если пользователь заходит по адресу /, направь его в
   # контроллер users, действие index. Грубо говоря, на главной странице у нас
   # список юзеров.
   root to: 'users#index'
+
+  # Эти две строчки добавляют ресурсы для пользователей и вопросов. Ресурс — это
+  # набор путей для управления соответствующей моделью. Посмотрите, что напишет
+  # команда
+  resources :users, except: [:destroy]
+  resources :sessions, only: [:destroy, :create, :new]
+  resources :questions, except: [:index, :new, :show]
+
+  get 'sign_up' => 'users#new'
+  get 'log_out' => 'sessions#destroy'
+  get 'log_in' => 'sessions#new'
 end
