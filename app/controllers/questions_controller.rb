94ddc1ced171cfe3f670db396cd127bbdba851f8
ukrmaxim <ukrmaxim@protonmail.com>
2021-04-21 16:09:52 +0300

    Fix display of question author and user page without questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -9,7 +9,7 @@ class QuestionsController < ApplicationController
     if @question.save
       redirect_to user_path(@question.user), notice: 'Вопрос задан'
     else
-      redirect_to user_path(@question.user), notice: 'Вопрос не может быть пустым!'
+      render :edit
     end
   end
 
