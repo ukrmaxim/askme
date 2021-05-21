15607a40f2720f639e1356d036c9c94e154141cf
ukrmaxim <ukrmaxim@protonmail.com>
2021-05-21 18:37:49 +0300

    Fix routes security

--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,7 @@ Rails.application.routes.draw do
   # Эти две строчки добавляют ресурсы для пользователей и вопросов. Ресурс — это
   # набор путей для управления соответствующей моделью. Посмотрите, что напишет
   # команда
-  resources :users
-  resources :sessions, only: [:destroy, :create, :new]
+  resources :users, except: [:destroy]
+  resource :session, only: [:destroy, :create, :new]
   resources :questions, except: [:index, :new, :show]
-
-  get 'sign_up' => 'users#new'
-  get 'log_out' => 'sessions#destroy'
-  get 'log_in' => 'sessions#new'
 end
