df90a937658f0821aa64dca9623d9a0f96280334
ukrmaxim <ukrmaxim@protonmail.com>
2021-06-10 22:12:58 +0300

    Add redirect to 404 page if no questions with hashtag

--- app/controllers/hashtags_controller.rb
+++ app/controllers/hashtags_controller.rb
@@ -1,5 +1,9 @@
 class HashtagsController < ApplicationController
   def show
     @hashtag = Hashtag.find_by!(name: params[:name])
+
+    unless @hashtag.questions.present?
+      render :file => "#{Rails.root}/public/404.html", layout: false, status: :not_found
+    end
   end
 end
